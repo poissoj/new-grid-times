323db7ac05175be7ae7c0872ead096d88a57c23e
Jean-Marie Poissonnier <jmp@jm-it.fr>
2022-05-23 22:07:46 +0200

    Exercice 1: Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { FAMILIES, QUERIES } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
   return (
@@ -29,7 +29,19 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <ActionGroupLaptop>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </ActionGroupLaptop>
         <Logo />
+        <SubscribeGroup>
+          <Button>Subscribe</Button>
+          <Anchor>Already a subscriber?</Anchor>
+        </SubscribeGroup>
       </MainHeader>
     </header>
   );
@@ -39,6 +51,9 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +74,45 @@ const ActionGroup = styled.div`
   }
 `;
 
+const ActionGroupLaptop = styled(ActionGroup)`
+  display: none;
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+  }
+`;
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+  @media ${QUERIES.tabletAndUp} {
+    margin-top: 48px;
+    margin-bottom: 84px;
+  }
+  @media ${QUERIES.laptopAndUp} {
+    justify-content: space-between;
+    margin-top: 16px;
+    margin-bottom: 72px;
+  }
+`;
+
+const SubscribeGroup = styled.div`
+  display: none;
+  @media ${QUERIES.laptopAndUp} {
+    display: grid;
+    gap: 8px;
+    justify-items: center;
+  }
+`;
+
+const Anchor = styled.a`
+  font-family: ${FAMILIES.serif};
+  font-style: italic;
+  font-size: 0.875rem;
+  text-decoration: underline;
+  color: var(--color-gray-900);
 `;
 
 export default Header;
